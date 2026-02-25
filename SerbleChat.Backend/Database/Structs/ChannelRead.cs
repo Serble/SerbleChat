d1a8b9b92ab21f3de634d5cf0120fbc7a2f0ae16
CoPokBl <copokbl@serble.net>
2026-02-25 18:34:39 +1100

    Fix db constraint error for channel reads

--- SerbleChat.Backend/Database/Structs/ChannelRead.cs
+++ SerbleChat.Backend/Database/Structs/ChannelRead.cs
@@ -13,11 +13,9 @@ public class ChannelRead {
     [StringLength(64), ForeignKey(nameof(UserNavigation))]
     public string UserId { get; set; } = null!;
     
-    [ForeignKey(nameof(LastReadMessageNavigation))]
     public int LastReadMessageId { get; set; }
     
     // Navigation Properties
     public Channel ChannelNavigation { get; set; } = null!;
     public ChatUser UserNavigation { get; set; } = null!;
-    public Message LastReadMessageNavigation { get; set; } = null!;
 }
